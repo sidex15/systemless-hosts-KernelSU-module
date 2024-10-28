d030880b60915496f59a123abd2029c7e637fd9e
sidex15 <moses.limbo@gmail.com>
2024-10-28 23:12:37 +0800

    Add SUSFS Hide support

--- service.sh
+++ service.sh
@@ -1,2 +1,5 @@
 MODDIR="${0%/*}"
+SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
 mount -t overlay -o lowerdir=/system/etc,upperdir=$MODDIR/system/etc,workdir=$MODDIR/worker KSU /system/etc
+${SUSFS_BIN} add_sus_mount /system/etc
+${SUSFS_BIN} add_try_umount /system/etc 1
